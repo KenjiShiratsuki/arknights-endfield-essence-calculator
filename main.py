d0e4639440032655bd10e37e69b39894c6d0a4d0
KenjiShiratsuki <kenjishiratsuki@gmail.com>
2026-05-28 19:55:12 -0400

    Version 1.0 complete. Functional, but does not print extra weapon essences that may drop for weapons NOT being considered so "perfect" essences may still be wasted. Includes all weapons and areas in Endfield Version 1.2

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ def translate_user_weapon_input(number_string, weapons):
     w_list = []
     for number in user_numbers:
         weapon = int(number.strip())
-        w_list.append(weapons[(weapon-1)])
+        w_list.append(weapons['data'][(weapon-1)])
     return w_list
 
-def find_optimal_settings(areas, matches, weapons, target_weapons):
+def find_optimal_settings(matches, target_weapons):
     all_cases = {}
     for area in matches['data'].keys():
-        area_weapons = matches['data'][area]['Valid Weapons']
+        area_weapons = matches['data'][area]['Valid_Weapons']
         target_attributes = []
         target_skills = []
         target_stats = []
@@ -33,18 +33,40 @@ def find_optimal_settings(areas, matches, weapons, target_weapons):
                 if weapon.get('attribute') not in target_attributes: target_attributes.append(weapon.get('attribute'))
                 if weapon.get('skill') not in target_skills: target_skills.append(weapon.get('skill'))
                 if weapon.get('stat') not in target_stats: target_stats.append(weapon.get('stat'))
-        while target_attributes < 3:
+        while len(target_attributes) < 3:
             target_attributes.append('free')
         for combo in combinations(target_attributes, 3):
-            for skill in target_skills:
-                count = 0
-                case_key = f"{area} | {combo} | {skill}"
-                matching_weapon_list = []
-                for weapon in target_weapons:
-                    if all(weapon.get('attribute') in combo, weapon.get('skill') == skill):
-                        count += 1
-                        all_cases[case_key] = {'count': count, 'weapons': matching_weapon_list}
+            for lock_type, lock_options in [('skill', target_skills), ('stat', target_stats)]:
+                for lock_name in lock_options:
+                    count = 0
+                    matching_weapon_list = []
+                    case_key = f"{area} | {combo} | {lock_type.upper()}: {lock_name}"
+                    
+                    for weapon in target_weapons:
+                        if weapon.get('attribute') in combo and weapon.get(lock_type) == lock_name:
+                            count += 1
+                            matching_weapon_list.append(weapon['name'])
+                    
+                    if count > 0:
+                        all_cases[case_key] = {
+                            'count': count, 
+                            'matching_weapons': matching_weapon_list
+                        }
+    return sorted(all_cases, key=lambda k: all_cases[k]['count'], reverse=True), all_cases
 
+def format_solution(sorted_solutions, all_cases, target_weapons):
+    remaining_names = [w['name'] for w in target_weapons]
+    output_string = "Endministrator, here are the results. For optimal essence farming, go to the following areas with the given settings to get the listed weapon's essences."
+    for case_key in sorted_solutions:
+        if not remaining_names:
+            break
+        case_data = all_cases[case_key]
+        weapon_list = [w for w in case_data['matching_weapons'] if w in remaining_names]
+
+        if weapon_list:
+            output_string += f"\n{case_key} | for -> {', '.join(weapon_list)}"
+            remaining_names = [w for w in remaining_names if w not in weapon_list]
+    return output_string
 
 def main():
     if not os.path.exists("weapons.json") or not os.path.exists("areas.json"):
@@ -64,5 +86,6 @@ def main():
         generate_matches()
     print(format_weapons(weapons['data']))
     target_weapons = translate_user_weapon_input(input(f"Please input which weapon numbers you want to focus on, separated by commas.\nEX: '1, 5, 32' will request focus on {weapons['data'][0]['name']}, {weapons['data'][4]['name']}, and {weapons['data'][31]['name']}: "), weapons)
-    find_optimal_settings(areas, matches, weapons, target_weapons)
+    solutions_by_score, all_cases = find_optimal_settings(matches, target_weapons)
+    print(format_solution(solutions_by_score, all_cases, target_weapons))
 main()
